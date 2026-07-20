6550f2b5a07273e45dd162819a6ebb17aeeb6357
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-20 15:43:14 +0600

    Update And Check Data is Loaded

--- src/train.py
+++ src/train.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 # Load dataset
 df = pd.read_csv("data/sensor_data.csv")
@@ -31,3 +33,44 @@ print(X.head())
 print("=" * 50)
 print("Target (y)")
 print(y.head())
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("=" * 50)
+print("Training Data:", len(X_train))
+
+print("Testing Data:", len(X_test))
+
+print("=" * 50)
+print("Training Decision Tree Model..")
+
+#Create Model
+model = DecisionTreeClassifier(random_state=42)
+
+
+# Train Model
+model.fit(X_train, y_train)
+
+print("Model Training Completed Successfuly!")
+
+print("=" * 50)
+print("Making Decision")
+
+predictions = model.predict(X_test)
+
+print("Predicted Values : ")
+print(predictions)
+
+print("Actual Values :")
+print(y_test.values)
+
+
+accuracy = accuracy_score(y_test, predictions)
+
+print("=" * 50)
+print(f"Model Accuracy: {accuracy * 100:.2f}%")
