18f23c71e5c865b1d29c04fcd6c47ac9dcc7aaf2
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-20 14:57:48 +0600

    Update And Check Data is Loaded

--- src/train.py
+++ src/train.py
@@ -3,6 +3,26 @@ import pandas as pd
 # Load dataset
 df = pd.read_csv("data/sensor_data.csv")
 
+print("=" * 50)
+print("Dataset Shape")
+print(df.shape)
+
+print("=" * 50)
+print("Column Names")
+print(df.columns)
+
+print("=" * 50)
+print("First 5 Rows")
 print(df.head())
-print(df.info())
+
+print("=" * 50)
+print("Missing Values")
+print(df.isnull().sum())
+
+print("=" * 50)
+print("Air Quality Count")
+print(df["AirQuality"].value_counts())
+
+print("=" * 50)
+print("Statistics")
 print(df.describe())
