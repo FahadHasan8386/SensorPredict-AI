c2a6c3f64d06329d460340c34e9ca06a03d1cde9
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-21 17:20:39 +0600

    Add predict

--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,8 @@
 import joblib
+import pandas as pd
 
+# Load Model
 model = joblib.load("model/model.pkl")
-
-temperature = float(input("Temperature: "))
-humidity = float(input("Humidity: "))
-mq7 = int(input("MQ7: "))
-mq136 = int(input("MQ136: "))
-
-prediction = model.predict([[temperature, humidity, mq7, mq136]])
 
 labels = {
     0: "Good",
@@ -15,5 +10,22 @@ labels = {
     2: "Poor"
 }
 
+print("=" * 50)
+print("Air Quality Prediction")
+print("=" * 50)
+
+temperature = float(input("Temperature: "))
+humidity = float(input("Humidity: "))
+mq7 = int(input("MQ7: "))
+mq136 = int(input("MQ136: "))
+
+input_data = pd.DataFrame(
+    [[temperature, humidity, mq7, mq136]],
+    columns=["Temperature", "Humidity", "MQ7", "MQ136"]
+)
+
+prediction = model.predict(input_data)
+
+print("\nPrediction Result")
 print("=" * 50)
 print("Predicted Air Quality:", labels[prediction[0]])
